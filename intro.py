0b6746e82ce33b1f87e8195e350c97b32188a326
Parthpanwar1 <panwarparth1289@gmail.com>
2026-01-11 09:07:35 +0530

    Add intro.py file

---
+++ intro.py
@@ -0,0 +1,19 @@
+#This is our python statement 
+print("Hello world")
+'''print() - is a built in function
+Quotation marks "" is only for text and special symbols'''
+
+#There is no need of quptation for numeric values
+print(25)
+
+# we use print() function as calculator.
+print(30*10)
+
+# python ignored the text which are written after '#' suymbols for example:
+# python ignore this line 
+print("But python read this line")
+
+#comments are use for Temporarily hiding or disabling code 
+'''print("I am learning python")
+print("Good luck")
+print("subscribe this channel")'''
